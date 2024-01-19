0f15c32928898f83f756064c0066b65dd3fe10b6
Olga Vikenteva <oleovic@rambler.ru>
2024-01-19 16:06:29 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TestExample
+{
+    public class Program
+    {
+        public static int Sum(int x , int y)
+        {
+            return x + y;
+        }
+        static void Main(string[] args)
+        {
+            int x = Sum(1, 3);
+            Console.WriteLine(x);
+        }
+    }
+}
